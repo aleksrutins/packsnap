90b9c64f506521f09b9389418ae37c9b5111f8ca
Aleks Rutins <aleks@rutins.com>
2022-12-21 11:07:40 -0500

    Add vala-language-server

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "A reproducible container build system";
   
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.vls.url = "github:aleksrutins/nix-vls";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, vls }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
@@ -18,10 +19,22 @@
                 pkgs.pkg-config
                 pkgs.meson
                 pkgs.ninja
+              ];
+              buildInputs = [
                 pkgs.glib
               ];
               src = self;
             };
+        };
+        devShells = flake-utils.lib.flattenTree {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              vls.packages.${system}.default
+            ];
+            buildInputs = [
+              packages.default
+            ];
+          };
         };
       }
     );
