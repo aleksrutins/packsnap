35bf48f89a4091b973595f297ae7ef43f9f4ccf0
Aleks Rūtiņš <aleks@rutins.com>
2022-12-22 17:31:37 -0500

    Make it work on Linux

--- lib/plan/default.nix
+++ lib/plan/default.nix
@@ -4,32 +4,9 @@
   buildPlan = variables: contents: libraries: runCmd:
     with builtins;
     {
-      extraCommands = ''
-        if [ -d $HOME/.nix-profile/etc/profile.d ]; then
-          for i in $HOME/.nix-profile/etc/profile.d/*.sh; do
-            if [ -r $i ]; then
-              . $i
-            fi
-          done
-        fi
-      '';
-      copyToRoot = 
-        pkgs.buildEnv {
-          name = "image-root";
-          paths = contents;
-          pathsToLink = ["/bin" "/lib"];
-        };
+      inherit contents;
       config = {
-        Env = 
-          let 
-          libraryPaths = (pkgs.lib.makeLibraryPath libraries);
-          setLibraryPaths = ''LD_LIBRARY_PATH="${libraryPaths}:$LD_LIBRARY_PATH"'';
-          in (concatLists [
-              [
-                setLibraryPaths
-              ] 
-              variables
-            ]);
+        Env = variables;
         Cmd = runCmd;
       };
     };
