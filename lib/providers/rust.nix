a619b2b719cdfe879eb75d2d2c55874d1e356ba7
Aleks Rūtiņš <aleks@rutins.com>
2022-12-22 17:40:26 -0500

    Basic Rust start command detection

--- lib/providers/rust.nix
+++ lib/providers/rust.nix
@@ -2,13 +2,16 @@
 , pkgs ? import sources.nixpkgs { overlays = [ (import sources."rust.nix") ]; }
 }:
 let plan = import ../plan {};
+    parseCargoToml = path:
+      pkgs.lib.importTOML /./${path}/Cargo.toml;
+    getStartCmd = path:
+      (parseCargoToml path).package.name or "";
 in {
   detect = path:
     builtins.pathExists (/./${path}/Cargo.toml);
-  
   plan = path:
     let
       derivation = pkgs.rust-nix.buildPackage { root = path; buildInputs = [pkgs.libiconv]; };
     in
-      plan.buildPlan [] [derivation] [] "";
+      plan.buildPlan [] [derivation] [] (getStartCmd path);
 }
