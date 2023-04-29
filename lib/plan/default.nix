d005b255ba49a73b60f5efc3fb2d5c753b863452
Aleks Rutins <aleks@rutins.com>
2023-04-29 08:09:05 -0400

    Use copyToRoot + buildEnv instead of contents

--- lib/plan/default.nix
+++ lib/plan/default.nix
@@ -4,7 +4,10 @@
   buildPlan = variables: contents: libraries: runCmd:
     with builtins;
     {
-      contents = concatLists [contents [pkgs.bash]];
+      copyToRoot = pkgs.buildEnv {
+        name = "packsnap-image";
+        paths = concatLists [contents [pkgs.bash]];
+      };
       config = {
         Env = variables;
         Cmd = ["${pkgs.bash}/bin/bash" "-c" runCmd];
