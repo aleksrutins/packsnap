c01dee8711bacd0571a84c9e8eb25ee1baa999db
Aleks Rutins <aleks@rutins.com>
2023-05-17 11:41:41 -0400

    JSON building DSL

--- oci/descriptor.c
+++ oci/descriptor.c
@@ -1,7 +1,7 @@
 #include <oci/descriptor.h>
 #include <string.h>
 #include <util/strsafe.h>
-#include <util/strutil.h>
+#include <util/json/builder.h>
 #include <util/json/basic_types.h>
 #include <stdlib.h>
 
@@ -10,45 +10,24 @@ void oci_content_descriptor_to_json(struct oci_content_descriptor *desc, char **
 
     clrstr(out_buf);
 
-    strcat_s(out_buf, "{ \"mediaType\": \"");
-    strcat_s(out_buf, desc->media_type);
-    strcat_s(out_buf, "\"");
+    json_build_property_str(out_buf, mediaType, desc->media_type)
 
-    strcat_s(out_buf, ", \"digest\": \"");
-    strcat_s(out_buf, desc->digest);
-    strcat_s(out_buf, "\"");
+    json_build_property_str(out_buf, digest, desc->digest)
 
-    itoa_buf = i64toa(desc->size);
-    strcat_s(out_buf, ", \"size\": \"");
-    strcat_s(out_buf, itoa_buf);
-    strcat_s(out_buf, "\"");
-    free(itoa_buf);
+    json_build_property_toa(out_buf, size, desc->size, i64)
 
-    if(desc->urls != NULL) {
-        char *urls_buf;
-        array_to_json(desc->urls, &urls_buf);
-        strcat_s(out_buf, ", \"urls\": ");
-        strcat_s(out_buf, urls_buf);
-        free(urls_buf);
-    }
+    if(desc->urls != NULL)
+        json_build_property_array(out_buf, urls, desc->urls)
 
-    char *annotations_buf;
-    map_to_json(desc->annotations, &annotations_buf);
-    strcat_s(out_buf, ", \"annotations\": ");
-    strcat_s(out_buf, annotations_buf);
-    free(annotations_buf);
+    json_build_property_map(out_buf, annotations, desc->annotations)
 
     if(desc->data != NULL) {
-        strcat_s(out_buf, ", \"data\": \"");
-        strcat_s(out_buf, desc->data);
-        strcat_s(out_buf, "\"");
+        json_build_property_str(out_buf, data, desc->data)
     }
 
     if(desc->artifact_type != NULL) {
-        strcat_s(out_buf, ", \"artifactType\": \"");
-        strcat_s(out_buf, desc->artifact_type);
-        strcat_s(out_buf, "\"");
+        json_build_property_str(out_buf, artifactType, desc->artifact_type)
     }
 
-    strcat_s(out_buf, "}");
+    json_end_object(out_buf)
 }
